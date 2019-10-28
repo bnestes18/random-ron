fd029c02644c09e10d23e098f44a0651724807c6
Brittney Estes <bnestes2014@gmail.com>
2019-10-28 16:26:40 -0400

    Add functionality to remove duplicate quotes

--- randomRon.js
+++ randomRon.js
@@ -1,21 +1,35 @@
 ;(function() {
 
+    const quoteSize = 50;
     let quoteEl = document.querySelector('blockquote');
     let button = document.querySelector('button');
+    let quotesArray = [];
+    
+
 
     function getQuote() {
         fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response)
-            }
-        }).then(function (data) {
-            quoteEl.textContent = data[0];
-        }).catch(function (err) {
-            quoteEl.textContent = 'Uh-oh. Something went wrong!';
-        })
+            .then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return Promise.reject(response)
+                }
+            }).then(function (data) {
+                generateQuotes(data);
+                quoteEl.textContent = data[0];
+            }).catch(function (err) {
+                quoteEl.textContent = 'Uh-oh. Something went wrong!';
+            })
+        }
+
+    function generateQuotes(data) { 
+        if (quotesArray.length === quoteSize) {
+            return quotesArray;
+        }
+        if (!quotesArray.includes(data[0])) {
+            quotesArray.push(data[0]);
+        }   
     }
 
     // Generate a quote on initial load
@@ -24,5 +38,4 @@
     // Generate another quote on click event
     button.addEventListener('click', getQuote, false);
     
-
 })();
