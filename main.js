e09fefac0fe12349d6299f7e9efbf969f113f0d3
bnestes18 <bnestes2014@gmail.com>
2021-06-29 22:18:00 -0400

    Refactor code to display status code when error occurs

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
+// VARIABLES
 let quote = document.querySelector('blockquote');
 let button = document.querySelector('button')
-console.log(quote)
 
+// FUNCTIONS
 function generateQuote() {
     fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes').then(function(response) {
         // If the response code is 200, return the json data
         if(response.ok) {
             return response.json();
         } else {
-            // Otherwise, return throw an error message
-            return response.text().then(function(json) {
-                throw json;
-            })
+            // Otherwise, throw an error status
+            throw response.statusText;
         }
     }).then(function(data) {
         quote.textContent = data[0];
     }).catch(function(error) {
-        // Render an error message to the DOM
+        // Render a basic error message to the DOM
         quote.textContent = `Error: ${error}`
     })
 }
@@ -24,4 +23,5 @@ function generateQuote() {
 // Generate quote on initial page load
 generateQuote();
 
+// EVENT LISTENERS
 button.addEventListener('click', generateQuote)
